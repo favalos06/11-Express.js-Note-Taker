38429c8d9ee808d27ef33a41757ee268db2d6647
Fernando Avalos <fernando.avalostorres91@gmail.com>
2022-06-07 15:08:09 -0700

    small route fixes

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -3,10 +3,9 @@ const router = require('express').Router()
 
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
-
 })
 
 ///need to confirm how this should function
-router.get("/index", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// router.get("/index", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
