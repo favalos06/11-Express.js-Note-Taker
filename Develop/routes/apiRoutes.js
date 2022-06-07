4af8d1cec2b942e744bcdaebde3e0a84a0e3e00e
Fernando Avalos <fernando.avalostorres91@gmail.com>
2022-06-07 15:01:18 -0700

    added some routes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
+
+router.post("/notes", (req, res) => {
+  const currentSaves = fs.readFileSync(path.join(process.cwd(), "/db/db.json"));
+
+  const newSaves = [
+    ...currentSaves,
+    { title: req.body.title, text: req.body.text, id: uuid() },
+  ];
+
+  fs.writeFileSync(path.join(process.cwd(), "/db/db.json"), newSaves);
+});
---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const router = require('express').Router()
+
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+
+})
+
+///need to confirm how this should function
+router.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
