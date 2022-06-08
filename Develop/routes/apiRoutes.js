80add13bab0b33f03a9f4355e7fb59cb8f745c75
Fernando Avalos <fernando.avalostorres91@gmail.com>
2022-06-07 18:09:26 -0700

    updated api routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,15 +1,32 @@
-const router = require("express").Router();
 const path = require("path");
+const router = require("express").Router();
 const fs = require("fs");
-const uuid = require("uuid");
+const uuid = require("../helpers/uuid");
+const savedNotes = require("../db/db.json");
 
-router.post("/notes", (req, res) => {
-  const currentSaves = fs.readFileSync(path.join(process.cwd(), "/db/db.json"));
-
-  const newSaves = [
-    ...currentSaves,
-    { title: req.body.title, text: req.body.text, id: uuid() },
-  ];
-
-  fs.writeFileSync(path.join(process.cwd(), "/db/db.json"), newSaves);
+router.get("/api/notes", (req, res) => {
+  res.json(savedNotes);
 });
+
+router.post("/api/notes", (req, res) => {
+  const title = req.body.title; 
+  const text = req.body.text;
+
+  if (title && text) {
+    const savedNotes = fs.readFileSync(path.join(process.cwd(), "/db/db.json"));
+    const parsedNotes = JSON.parse(savedNotes);
+    parsedNotes.push({
+      title,
+      text,
+      id: uuid(),
+    });
+
+    fs.writeFileSync(
+      path.join(process.cwd(), "db/db.json"),
+      JSON.stringify(parsedNotes)
+    );
+  }
+});
+
+// Export module
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const htmlRoutes = require("./routes/htmlRoutes")
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.use("/", htmlRoutes);
-
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
+
+app.use("/", apiRoutes);
+app.use("/", htmlRoutes);
